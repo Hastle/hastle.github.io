dcf5ed8623c8a9a4d0bca78da10b3b4f16bbe0b2
Alexander Egrerev <alexandrs.me.com@gmail.com>
2017-06-05 10:44:43 +0300

    add companents

---
+++ Idea/js/common.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	new WOW().init();
+
+
+	//Таймер обратного отсчета
+	//Документация: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+
+	});
+
+});
