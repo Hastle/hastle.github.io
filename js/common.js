bc622a1528c83429baa7214429f713c2a0a50c4f
Alexander Egrerev <alexandrs.me.com@gmail.com>
2017-06-22 20:03:35 +0300

    add texts

--- js/common.js
+++ js/common.js
@@ -2,37 +2,6 @@ $(document).ready(function() {
 
 	new WOW().init();
 
-	//Добавляет классы дочерним блокам .block для анимации
-	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
-
-	//Плавный скролл до блока .div по клику на .scroll
-	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
-		});
-	});
-
-	//Кнопка "Наверх"
-	//Документация:
-	//http://api.jquery.com/scrolltop/
-	//http://api.jquery.com/animate/
-	$("#top").click(function () {
-		$("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
-
-
-
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
 	$("form").submit(function() {
@@ -69,11 +38,11 @@ $(document).ready(function() {
 		if ($(".top_mnu").is(":visible")) {
 			$(".top_text").css("opacity", "1");
 			$(".top_mnu").fadeOut(600);
-			$(".top_mnu li a").removeClass("fadeInUp animated");
+			$(".top_mnu li a, .soc-links a i").removeClass("fadeInUp animated");
 		} else {
 			$(".top_text").css("opacity", ".1");
 			$(".top_mnu").fadeIn(600);
-			$(".top_mnu li a").addClass("fadeInUp animated");
+			$(".top_mnu li a, .soc-links a i").addClass("fadeInUp animated");
 		};
 	});
 
